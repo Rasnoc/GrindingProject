cbd1aebac7d31a09479b278ed5a7f132013f1ef4
Carson Jones <carsonator406@yahoo.com>
2021-11-30 21:15:59 -0600

    Basic Character Movement/Animation
    I've added basic character movement and the animations to go with it. Ramps and cubes have been added for testing collision, walking up slopes, and falling/jumping.

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public Animator animator;
+
+    public float walkSpeed = 1.5f;
+    public float sprintSpeed = 6f;
+    float speed;
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public LayerMask grindRailMask;
+    bool isGrounded;
+    bool isGrinding;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrinding = Physics.CheckSphere(groundCheck.position, groundDistance, grindRailMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -4f;
+            animator.SetBool("isJumping", false);
+        }
+
+
+        bool isSprinting = Input.GetKey("left shift");
+        if (isSprinting)
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if(!isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("isJumping", true);
+        }
+
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/animationStateController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    int isJumpingHash;
+    int isFallingHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        //This lets us type isWalkingHash instead of "isWalking" in order to increase performance. It's for optimization.
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        isJumpingHash = Animator.StringToHash("isJumping");
+        isFallingHash = Animator.StringToHash("isFalling");
+    }
+
+    void Update()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool movementPressed = Input.GetKey("w") | Input.GetKey("a") | Input.GetKey("s") | Input.GetKey("d");
+        bool runPressed = Input.GetKey("left shift");
+        //bool oppositeDirections = (Input.GetKey("w") & Input.GetKey("s")) | (Input.GetKey("a") && Input.GetKey("d"));
+
+        if (!isWalking && movementPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        if ((isWalking && !movementPressed))
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        if (!isRunning && (movementPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!movementPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+}
